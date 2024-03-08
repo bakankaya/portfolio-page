aa82cb7bd0a97a61593ee672e1593e1fb62fa995
bakankaya <batakankarakaya@gmail.com>
2024-03-08 12:01:10 +0300

    Added a Show More/Less function to Projects Section.

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState } from "react";
+import { Box, Button, Heading, HStack } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesDown, faAnglesUp } from "@fortawesome/free-solid-svg-icons";
 import FullScreenSection from "./FullScreenSection";
-import { Box, Heading, Img } from "@chakra-ui/react";
 import Card from "./Card";
 import img from "../images/photo1.jpg";
 
@@ -63,17 +66,36 @@ const projects = [
   },
 ];
 
-
-
 const ProjectsSection = () => {
+  const [itemList, setItemList] = useState(3);
+  const startElement = document.getElementById("section-start");
+  const endElement = document.getElementById("section-end");
+
+  function handleClick() {
+    if (itemList == 3) {
+      setItemList(projects.length);
+      endElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    } else {
+      setItemList(3);
+      startElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  }
+
   return (
     <FullScreenSection
       color="#e5e7eb"
       p={8}
       alignItems="flex-start"
       spacing={8}
+      id="section-start"
     >
-      <Heading as="h1" id="projects-section">
+      <Heading as="h1" id="projects-section" marginTop="5vh" alignSelf="center">
         Featured Projects
       </Heading>
       <Box
@@ -81,7 +103,7 @@ const ProjectsSection = () => {
         gridTemplateColumns="repeat(3,minmax(0,1fr))"
         gridGap={8}
       >
-        {projects.map((project) => (
+        {projects.slice(0, itemList).map((project) => (
           <Card
             key={project.title}
             title={project.title}
@@ -91,6 +113,21 @@ const ProjectsSection = () => {
           />
         ))}
       </Box>
+      <Button id="section-end" width="full" onClick={handleClick}>
+        {itemList === 3 ? (
+          <HStack spacing={2} alignItems="center">
+            <FontAwesomeIcon icon={faAnglesDown} size="1x" />
+            <p>Show More</p>
+            <FontAwesomeIcon icon={faAnglesDown} size="1x" />
+          </HStack>
+        ) : (
+          <HStack spacing={2} alignItems="center">
+            <FontAwesomeIcon icon={faAnglesUp} size="1x" />
+            <p>Show Less</p>
+            <FontAwesomeIcon icon={faAnglesUp} size="1x" />
+          </HStack>
+        )}
+      </Button>
     </FullScreenSection>
   );
 };
